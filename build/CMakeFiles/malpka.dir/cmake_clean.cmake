66704f47eee7cd5e885a906eebca2a05879c7b48
xlebyshek17 <kristina1111113@gmail.com>
2025-11-18 17:18:06 +0100

    initial commit

---
+++ build/CMakeFiles/malpka.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/malpka.dir/Malpka.cpp.o"
+  "CMakeFiles/malpka.dir/Malpka.cpp.o.d"
+  "CMakeFiles/malpka.dir/MalpkaMyFrame.cpp.o"
+  "CMakeFiles/malpka.dir/MalpkaMyFrame.cpp.o.d"
+  "CMakeFiles/malpka.dir/main.cpp.o"
+  "CMakeFiles/malpka.dir/main.cpp.o.d"
+  "malpka"
+  "malpka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/malpka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/malpka.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for malpka.
+# This may be replaced when dependencies are built.
